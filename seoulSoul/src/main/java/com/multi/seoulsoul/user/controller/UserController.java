2a61f7bbaea90c70409fab212c7c53ace376ae24
DESKTOP-5QIS43H\ljk13 <ljk1354@naver.com>
2024-06-21 22:31:24 +0900

    branch :: (재식 #1) users 브랜치 추가

--- seoulSoul/src/main/java/com/multi/seoulsoul/user/controller/UserController.java
+++ seoulSoul/src/main/java/com/multi/seoulsoul/user/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.multi.seoulsoul.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
 }
